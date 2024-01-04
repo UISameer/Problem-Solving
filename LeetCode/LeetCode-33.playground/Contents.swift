ad5010236b50bc61df2ce79a7d77073b6b864528
Sameer Jagtap <jagtap.sameer00@gmail.com>
2024-01-04 21:39:36 +0530

    Added LeetCode 33. Search in Rotated Sorted Array

---
+++ LeetCode/LeetCode-33.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        
+        while left <= right {
+            var mid = (left + right) / 2
+            // If target is mid return
+            if nums[mid] == target {
+                return mid
+            }
+            // Left sorted portion
+            else if nums[left] <= nums[mid] {
+                if target > nums[mid] || target < nums[left]{
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+            // Right sorted portion
+            else {
+                if target < nums[mid] || target > nums[right] {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            }
+        }
+         return -1
+    }
+}
+
+let nums = [4,5,6,7,0,1,2], target = 0
+print(Solution().search(nums, target))
